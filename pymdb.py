6e16c5b654a9c21aaa30aac7624f066cee814f9a
saksitdGtec <saksitd2021@gmail.com>
2021-07-16 11:39:14 +0700

    t3

--- pymdb.py
+++ pymdb.py
@@ -1,6 +1,13 @@
 import pyodbc
 
 conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb)};DBQ=E:\GrandSchedule_Coding\GrandSchedule\GrandSchedule.mdb;') #สำหรับ msaccess 32bit + python 32bit
+
+#สำหรับ py64bit + msaccess 64bit
+# conn_str = (
+#     r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+#     r'DBQ=C:\path\to\mydb.accdb;'
+#     )
+
 cursor = conn.cursor()
 cursor.execute(r'select * from tblDay where day_id = 2')
 
